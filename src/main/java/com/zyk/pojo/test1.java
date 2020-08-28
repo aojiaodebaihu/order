0b7e8d8c056dbc1aab5391e8ec9c4ef8515f6b86
aojiaodebaihu <2621824119@qq.com>
2020-08-28 18:14:41 +0800

    第一次提交

---
+++ src/main/java/com/zyk/pojo/test1.java
@@ -0,0 +1,9 @@
+package com.zyk.pojo;
+
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println("成功");
+    }
+
+
+}
---
+++ src/test/java/Test11.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+public class Test11 {
+
+    @Test
+    void test1(){
+
+
+        System.out.println("错误");
+    }
+}
